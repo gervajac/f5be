ea4cb276d48f2f8a3b9211f754cfadad8f1f9fb9
Gervasio Jacob <gervasio@crespodev.com>
2024-05-28 20:46:46 -0300

    cambios en main

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,10 @@ import { PlayersModule } from './players/players.module';
 import { MatchesModule } from './matches/matches.module';
 import { LeagueModule } from './league/league.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
-
-mongoose.set('debug', true); // Enable Mongoose debugging
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://gervajac:Gervajacob123@f5cluster.qvbv6qx.mongodb.net/?retryWrites=true&w=majority&appName=f5cluster', {
-      retryAttempts: 5, // Customize retry attempts
-      retryDelay: 3000, // Customize retry delay in milliseconds
-    }),
+    MongooseModule.forRoot('mongodb+srv://gervajac:Gervajacob123@f5cluster.qvbv6qx.mongodb.net'),
     PlayersModule,
     MatchesModule,
     LeagueModule,
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,14 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true }); // Habilitar CORS
+  const port = process.env.PORT || 3002; // Usar el puerto definido por el entorno o el puerto 3002
 
-  const port = 3002;
-  await app.listen(port);
-  
-  Logger.log(`Application is running on: ${await app.getUrl()}`, 'Bootstrap');
+  await app.listen(port, '0.0.0.0'); // Escuchar en todas las interfaces de red
+
+  Logger.log(`Application is running on: http://localhost:${port}`, 'Bootstrap');
 }
 
-bootstrap()
-  .catch(err => {
-    Logger.error('Error during application bootstrap', err);
-  });
+bootstrap().catch(err => {
+  Logger.error('Error during application bootstrap', err);
+});
